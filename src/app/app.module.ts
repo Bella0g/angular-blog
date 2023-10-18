a79d1a3f4f029520de5f11291b518e33fde03bcb
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-18 14:24:10 +0200

    added photo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/post-list/post-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+interface post {
+  title: string;
+  imageUrl: string;
+}
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  PostList: post[] = [
+    {
+      title: 'Bar opening in paris',
+      imageUrl: './assets/bar/bar-3.jpeg',
+    },
+    {
+      title: 'Ballerina photoshoot in NYC',
+      imageUrl: './assets/ballerina/ballerina-4.jpeg',
+    },
+    {
+      title: 'Designer behind the sencse',
+      imageUrl: './assets/designer/designer-4.jpeg',
+    },
+  {
+    title: 'Pasta',
+    imageUrl: './assets/india/india-hidden.jpeg',
+    },
+    {
+      title: 'Photoshoot for japans natinal day',
+      imageUrl: './assets/japan/japan-model.jpeg',
+    },
+  ];
+}
+
