667bdce72f5c8e1bf9a1a0a86e925ba8219d4fd8
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-18 20:34:36 +0200

    changed header styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MeComponent } from './components/me/me.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { HeaderComponent } from './components/header/header.component';
     LoginComponent,
     MainComponent,
     PostListComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    MeComponent
   ],
   imports: [
     BrowserModule
