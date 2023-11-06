bba89b26294acfd7cc16bd390c5cc29e1d7511f0
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-11-06 07:48:54 +0100

    changed css styling to fit material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import { AdminComponent } from './pages/admin/admin.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { CommentFormComponent } from './components/comment-form/comment-form.component';
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +38,10 @@ import { CommentFormComponent } from './components/comment-form/comment-form.com
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    
+    MatButtonModule,
+    MatFormFieldModule,
+     MatInputModule,
+      MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
